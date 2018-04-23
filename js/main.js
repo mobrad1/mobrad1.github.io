965ed567bb2f48bba557ba68709ac94d4e6acd1e
Chiemela Chinedum <chiemelachinedum@gmail.com>
2018-04-23 16:57:27 +0100

    Initial Commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+var options = {
+    strings: [
+        "develop <span class=\"yellow\">responsive</span> websites^1500",
+        "build <span class=\"yellow\">awesome</span> applications^1500", 
+        "research <span class=\"yellow\">solutions</span> to problems^1500"
+    ],
+    typeSpeed: 100,
+    smartBackspace: true,
+    loop: 'infinite',
+}
+  
+var typed = new Typed(".typed-desc", options);
